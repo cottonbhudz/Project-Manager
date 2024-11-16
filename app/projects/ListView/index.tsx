a410b38eb76b2a5ca300bc967ae0450367b5fe6b
Vir Chuy Darm <118208482+cottonbhudz@users.noreply.github.com>
2024-11-15 21:59:50 -0500

    Add files via upload

--- app/projects/ListView/index.tsx
+++ app/projects/ListView/index.tsx
@@ -1,4 +1,5 @@
 import Header from '@/components/Header';
+import TaskCard from "@/components/TaskCard";
 import { Task, useGetTasksQuery } from '@/state/api';
 import React from 'react'
 
@@ -17,16 +18,15 @@ const ListView = ({id, setIsModalNewTaskOpen}: Props) => {
     if (isLoading) return <div>Loading...</div>;
     if (error) return <div> An error occured while fetching tasks</div>;
 
-    return <div className="px-4 pb-8 xl:px-6">
+    return ( <div className="px-4 pb-8 xl:px-6">
         <div className='pt-5'>
             <Header name="List" />
         </div>
         <div className='grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 lg:gap-6'>
-            {tasks?.map((task:Task) => (
-                <TaskCard key={task.id} task={task} />
-            ))}
+            {tasks?.map((task:Task) => <TaskCard key={task.id} task={task} /> )}
         </div>
     </div>
-}
+    );
+};
 
 export default ListView

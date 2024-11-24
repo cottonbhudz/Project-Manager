729c567923aa63a1322ef852724d12e3872fe044
Vir Chuy Darm <118208482+cottonbhudz@users.noreply.github.com>
2024-11-23 22:56:29 -0500

    Add files via upload

--- app/projects/ProjectHeader.tsx
+++ app/projects/ProjectHeader.tsx
@@ -1,6 +1,7 @@
 import Header from '@/components/Header';
 import { Clock, Filter, Grid3x3, List, PlusSquare, Share2, Table } from "lucide-react";
 import React, { useState } from 'react'
+import ModalNewProject from "./ModalNewProject";
 
 type Props = {
     activeTab: string;
@@ -12,10 +13,10 @@ const ProjectHeader = ({activeTab, setActiveTab}: Props) => {
     
     return (
     <div className= "px-4 xl:px-6">
-        {/*<ModalNewProject
+        <ModalNewProject
           isOpen={isModalNewProjectOpen}
           onClose={() => setIsModalNewProjectOpen(false)} 
-        />*/}
+        />
         <div className="pb-6 pt-6 lg:pb-4 lg:pt-8">
             <Header name="Product Design Development" 
               buttonComponent={

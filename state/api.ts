28a514dc71e741e393e9d8ce7ec1b1aa4988b2e5
Vir Chuy Darm <118208482+cottonbhudz@users.noreply.github.com>
2024-11-27 21:20:56 -0500

    Add files via upload

--- state/api.ts
+++ state/api.ts
@@ -66,10 +66,17 @@ export interface SearchResults {
     users?: User[];
 }
 
+export interface Team {
+    teamId: number;
+    teamName: string;
+    productOwnerUserId?: number;
+    projectManagerUserId?: number;
+}
+
 export const api = createApi({
     baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL}),
     reducerPath: "api",
-    tagTypes: ["Projects", "Tasks"],
+    tagTypes: ["Projects", "Tasks", "Users", "Teams"],
     endpoints: (build) => ({
         getProjects: build.query<Project[], void>({
             query: () => "projects",
@@ -108,6 +115,14 @@ export const api = createApi({
                 { type: "Tasks", id: taskId},
             ],
         }),
+        getUsers: build.query<User[], void>({
+            query: () => "users",
+            providesTags: ["Users"]
+        }),
+        getTeams: build.query<Team[], void>({
+            query: () => "teams",
+            providesTags: ["Teams"]
+        }),
         search: build.query<SearchResults, string>({
             query: (query) => `search?query=${query}`
         })
@@ -121,4 +136,6 @@ export const {
     useCreateTaskMutation,
     useUpdateTaskStatusMutation,
     useSearchQuery,
+    useGetUsersQuery,
+    useGetTeamsQuery,
 } = api;
